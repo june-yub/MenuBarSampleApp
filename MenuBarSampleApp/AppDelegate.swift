8b35c363768f20aa30b362eb192be979d8eede0c
jeonjunyeop <john40644064@gmail.com>
2021-09-26 14:21:16 +0900

    Print the quote when the menu mar app is clicked..

--- MenuBarSampleApp/AppDelegate.swift
+++ MenuBarSampleApp/AppDelegate.swift
@@ -10,11 +10,29 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    
+    // This creates a Status Item - aka application icon - in the menu bar with a fixed length that the user will see and use..
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
 
+    @objc func printQuote(_sender: Any?)
+    {
+        let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+        let quoteAuthor = "Mark Twain"
+        
+        print("\(quoteText) - \(quoteAuthor)")
+    }
+    
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        if let button = statusItem.button
+        {
+            // for its icon
+            button.image = NSImage(named: NSImage.Name("StatusBarButtonImage"))
+            
+            // for the action when it is clicked..
+            button.action = #selector(printQuote(_sender:))
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
